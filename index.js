0fe7cf93a1b1c5a7dc5a38a309f430f4acb61a24
jocowl <jocowl25@gmail.com>
2025-09-09 23:32:38 -0400

    verification

--- index.js
+++ index.js
@@ -10,9 +10,12 @@ function ifThen(a,b){
 function onlyIf(a,b){
     return (a==b)
 }
-const alphabet="QWERTYUIOPASDFGHJKLZXCVBNM".split("")
+const alphabet="QWERTYUIOPASDFGHJKLZXCBNM".split("")
 const operators="~,->,<->,&,v,(,)".split(",")
+const innerOperators="->,<->,&,v".split(",")
 const letters=[];
+const premises=[]
+let conclusion;
 const varButton=document.querySelector('.varAddButton')
 const varlist=document.querySelector(".varList")
 const varInput=document.querySelector(".varAddInput")
@@ -41,18 +44,87 @@ concInput.addEventListener("change",()=>{
 
     premButton.addEventListener("click",()=>{
         premInput.value=premInput.value.toUpperCase()
-        let input=premInput.value.split("")
-        if(parsePremise(input)){
-            premList.innerHTML+=premInput.value+"<br>"
+        let input=premInput.value
+        input=parsePremise(input)
+        if(input){
+            premList.innerHTML+=input.join("")+"<br>"
+            premises.push(input)
+        }else{
+            alert("invalid stupid")
         }
         })
 
+        concButton.addEventListener("click",()=>{
+        concInput.value=concInput.value.toUpperCase()
+        let input=concInput.value
+        input=parsePremise(input)
+        if(input){
+            conclusion=input
+        }else{
+            alert("invalid stupid")
+        }
+        doTheThing()
+        })
+
+function doTheThing(){
+        console.log(premises)
+        console.log(conclusion)
+}
+
 function parsePremise(input){ //TO DO: fix this
-    for(let i=0;i<input.length;i++){
-            if(!letters.includes(input[i])&&!operators.includes(input[i])){
-                alert("invalid something or other")
-                return false;
+    let inputArr=input.split("")
+    for(let i=0;i<inputArr.length;i++){
+        if(inputArr[i]=="-"){
+            if(inputArr[i+1]==">"){
+                if(inputArr[i-1]=="<"){
+                    inputArr.splice(i-1,3,"<->")
+                }else{
+                    inputArr.splice(i,2,"->")
+                }
             }
         }
-    return true
+    }
+    let parenCount=0;
+    for(let i=0;i<inputArr.length;i++){
+        if(inputArr[i]=="V"){
+                inputArr[i]="v"
+            }
+        if(letters.includes(inputArr[i])){
+            if(letters.includes(inputArr[i-1])||letters.includes(inputArr[i+1])){
+                alert(`adjacent letters! see char ${i}`)
+                return false;
+            }
+            if(inputArr[i-1]=="("&&inputArr[i+1]==")"){
+                alert(`parens around only letter! see char ${i}`)
+                return false
+            }
+            if((inputArr[i-1]!="~"&&inputArr[i-1]!="(")&&innerOperators.includes(inputArr[i+1])){
+                alert(`invalid starting operator! see char ${i}`)
+                return false
+            }
+            continue;
+        }
+        if(operators.includes(inputArr[i])){
+            if((operators.includes(inputArr[i-1]))||operators.includes(inputArr[i+1])){
+                if(!( (inputArr[i]!="~"&&inputArr[i+1]!="(") || (inputArr[i-1]!="~"&&inputArr[i]!="(") )){
+                alert(`adjacent operations! see char ${i}`)
+                return false;
+                }
+            }
+            if(inputArr[i]=="("){
+                parenCount++
+            }
+            if(inputArr[i]==")"){
+                parenCount--
+            }
+            continue;
+        }
+        alert(`invalid symbol! see char ${i}`)
+        return false
+}
+if(parenCount!=0){
+        alert(`paren count off! see char ${i}`)
+    return false;
+}
+return inputArr;
 }
